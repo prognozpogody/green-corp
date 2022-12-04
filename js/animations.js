84e50180c5f86bdff2e62f0ba8fa0b2f97623cc6
gennadiy <prognozpogody@bk.ru>
2022-12-04 17:22:28 +0300

    add Event

--- js/animations.js
+++ js/animations.js
@@ -23,3 +23,46 @@ function initIncreaseNumberAnimation() {
 }
 
 initIncreaseNumberAnimation();
+
+document
+  .querySelector("#budget")
+  .addEventListener("change", function handleSelectChange(event) {
+    if (event.target.value === "other") {
+      const formContainer = document.createElement("div");
+      formContainer.classList.add("form__group");
+      formContainer.classList.add("form__other-input");
+
+      const input = document.createElement("input");
+      input.placeholder = "Введите ваш вариант";
+      input.type = "text";
+
+      formContainer.appendChild(input);
+      document
+        .querySelector(".form form")
+        .insertBefore(formContainer, document.querySelector(".form__submit"));
+    }
+
+    const otherInput = document.querySelector(".form__other-input");
+    if (event.target.value !== "other" && otherInput) {
+      document.querySelector(".form form").removeChild(otherInput);
+    }
+  });
+
+function updateScroll() {
+  if (window.scrollY > 0) {
+    document.querySelector("header").classList.add("header__scrolled");
+  } else {
+    document.querySelector("header").classList.remove("header__scrolled");
+  }
+
+  let windowBottomPosition = window.scrollY + window.innerHeight;
+  let countElementPosition = document.querySelector(
+    ".features__clients-count"
+  ).offsetTop;
+  if (windowBottomPosition >= countElementPosition && !animationInited) {
+    animationInited = true;
+    initIncreaseNumberAnimation();
+  }
+}
+
+window.addEventListener("scroll", updateScroll);
